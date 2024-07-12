803254826afeb2bad9f9dae246420c0406c8ad9a
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-07-12 14:16:20 -0300

    after class

---
+++ afterclass/population_db.sql
@@ -0,0 +1,35 @@
+USE import_win;
+
+SET GLOBAL local_infile = true;
+
+LOAD DATA LOCAL INFILE './data/dataset.csv' 
+INTO TABLE import_win.vehiculo_collision 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\r\n' 
+IGNORE 1 ROWS
+(   @crash_date
+,   @borough
+,   @zip_code
+,   @vehicle
+,   @vehicle_type)
+SET 
+    crash_date = NULLIF(@crash_date,'')
+,   borough = NULLIF(@borough,'')
+,   zip_code = NULLIF(@zip_code,'')
+,   vehicle = NULLIF(@vehicle,'')
+,   vehicle_type = NULLIF(@vehicle_type,'')
+;
+
+
+
+
+
+
+SELECT
+    crash_date
+,   borough
+,   zip_code
+,   vehicle
+,   vehicle_type
+FROM import_win.vehiculo_collision LIMIT 100;
